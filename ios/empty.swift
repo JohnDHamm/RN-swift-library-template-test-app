47f049672bdc9b799d12e8b4325ddfd7802b0eb9
John Hamm <johndhamm1@gmail.com>
2019-02-22 19:04:04 -0600

    first commit

---
+++ ios/empty.swift
@@ -0,0 +1,9 @@
+//
+//  empty.swift
+//  ReactNativeSwiftLibraryTemplateTestApp
+//
+//  Created by John Hamm on 2/22/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
---
+++ templates.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const { RNSwiftLibraryTemplate } = require('@johndhammcodes/react-native-swift-library-template');
+
+export default class Templates extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      callbackMsg: "",
+      promiseResponse: "",
+      constant: ""
+    }
+  }
+
+  componentDidMount() {
+    console.log("test", RNSwiftLibraryTemplate)
+    RNSwiftLibraryTemplate.callbackMethod(res => {
+      console.log("callback response: ", res);
+      this.setState({callbackMsg: res});
+    })
+    this.setState({constant: RNSwiftLibraryTemplate.constant})
+    RNSwiftLibraryTemplate.promiseMethod()
+      .then(res => {
+        console.log("promise response: ", res);
+        this.setState({promiseResponse: res})
+      })
+      .catch(e => {
+        console.log("promise error: ", e.message, e.code);
+        this.setState({promiseResponse: e.message})
+      })
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>templates:</Text>
+        <Text>callback: {this.state.callbackMsg}</Text>
+        <Text>constant: {this.state.constant}</Text>
+        <Text>promise response: {this.state.promiseResponse}</Text>
+      </View>
+    )
+  }
+}
